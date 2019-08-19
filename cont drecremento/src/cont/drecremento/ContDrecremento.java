d702a575e0cf500c7bf76fde36c266ca8079b2dc
Joabe5277 <54293241+Joabe5277@users.noreply.github.com>
2019-08-19 20:39:19 -0300

    Add files via upload

---
+++ cont drecremento/src/cont/drecremento/ContDrecremento.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cont.drecremento;
+
+/**
+ *
+ * @author aluno
+ */
+public class ContDrecremento {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+        cont = 10;
+        while(cont >=0){
+            System.out.println("Valor de cont:"+ cont);
+            cont = cont -1;
+    ]
+}
